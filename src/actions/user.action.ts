a375bf3376781ae5cb8c33b33d8d257e38931411
lipika1911 <lipikaarya1723@gmail.com>
2025-08-05 19:52:40 +0530

    feat: added server actions and components for liking, commenting, and deleting posts

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -56,7 +56,7 @@ export async function getUserByClerkId(clerkId:string){
 
 export async function getDbUserId(){
     const {userId: clerkId} = await auth();
-    if(!clerkId) throw new Error("Unauthorized");
+    if(!clerkId) return null;
 
     const user = await getUserByClerkId(clerkId);
     if(!user) throw new Error("User not found");
@@ -67,6 +67,8 @@ export async function getDbUserId(){
 export async function getRandomUsers(){
     try {
         const userId = await getDbUserId();
+
+        if(!userId) return [];
 
         //get 3 random users exclude ourselves and the users we already follow
         const randomUsers = await prisma.user.findMany({
